f11946c635a7db8798d3bf7f69a510ac40e92cfd
yanagitani <takanori.yanagitani@gmail.com>
2020-01-04 10:53:54 +0900

    many small tests.

--- proc2json_test.go
+++ proc2json_test.go
@@ -6,20 +6,13 @@ import (
 	"encoding/json"
 )
 
-func TestConvert(t *testing.T) {
+func TestConvert_Environ(t *testing.T) {
 	p := Proc{
 		Environ: []string{
 			"LANG=en_US.UTF-8",
 			"TERM=screen",
 			"SHELL=/bin/bash",
 		},
-		Cmdline: []string{
-			"vim",
-			"proc2json.go",
-		},
-		SessionId: "5",
-		Status:    "Name: vim\nnonvoluntary_ctxt_switches: 42\n",
-		Limits:    "Limit SoftLimit HardLimit Units\nMax cpu time unlimited unlimited seconds\n",
 	}
 	j, e := Convert(&p)
 	switch e {
@@ -55,6 +48,30 @@ func TestConvert(t *testing.T) {
 	if "SHELL=/bin/bash" != a.Environ[2] {
 		t.Errorf("SHELL mismatch.\n")
 	}
+}
+
+func TestConvert_Cmdline(t *testing.T) {
+	p := Proc{
+		Cmdline: []string{
+			"vim",
+			"proc2json.go",
+		},
+	}
+	j, e := Convert(&p)
+	switch e {
+	case nil:
+		break
+	default:
+		t.Errorf("Marshal error: %v\n", e)
+	}
+	a := Proc{}
+	em := json.Unmarshal(j, &a)
+	switch em {
+	case nil:
+		break
+	default:
+		t.Errorf("Unmarshal error: %v\n", em)
+	}
 
 	switch len(a.Cmdline) {
 	case 2:
@@ -70,13 +87,76 @@ func TestConvert(t *testing.T) {
 	if "proc2json.go" != a.Cmdline[1] {
 		t.Errorf("arg1 mismatch.\n")
 	}
+}
+
+func TestConvert_SessionId(t *testing.T) {
+	p := Proc{
+		SessionId: "5",
+	}
+	j, e := Convert(&p)
+	switch e {
+	case nil:
+		break
+	default:
+		t.Errorf("Marshal error: %v\n", e)
+	}
+	a := Proc{}
+	em := json.Unmarshal(j, &a)
+	switch em {
+	case nil:
+		break
+	default:
+		t.Errorf("Unmarshal error: %v\n", em)
+	}
 
 	if "5" != a.SessionId {
 		t.Errorf("sessionid mismatch.\n")
 	}
+}
+
+func TestConvert_Status(t *testing.T) {
+	p := Proc{
+		Status:    "Name: vim\nnonvoluntary_ctxt_switches: 42\n",
+	}
+	j, e := Convert(&p)
+	switch e {
+	case nil:
+		break
+	default:
+		t.Errorf("Marshal error: %v\n", e)
+	}
+	a := Proc{}
+	em := json.Unmarshal(j, &a)
+	switch em {
+	case nil:
+		break
+	default:
+		t.Errorf("Unmarshal error: %v\n", em)
+	}
 
 	if "Name: vim\nnonvoluntary_ctxt_switches: 42\n" != a.Status {
 		t.Errorf("Status mismatch.\n")
+	}
+}
+
+func TestConvert_Limits(t *testing.T) {
+	p := Proc{
+		Limits:    "Limit SoftLimit HardLimit Units\nMax cpu time unlimited unlimited seconds\n",
+	}
+	j, e := Convert(&p)
+	switch e {
+	case nil:
+		break
+	default:
+		t.Errorf("Marshal error: %v\n", e)
+	}
+	a := Proc{}
+	em := json.Unmarshal(j, &a)
+	switch em {
+	case nil:
+		break
+	default:
+		t.Errorf("Unmarshal error: %v\n", em)
 	}
 
 	if "Limit SoftLimit HardLimit Units\nMax cpu time unlimited unlimited seconds\n" != a.Limits {
